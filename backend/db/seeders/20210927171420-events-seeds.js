9c654332e73e0e920dbf2c3ece4b1563cc856398
daniel <penced0513@gmail.com>
2021-10-03 17:28:53 -0500

    added invite entire group, more css, added link to github readme on login/signup

--- backend/db/seeders/20210927171420-events-seeds.js
+++ backend/db/seeders/20210927171420-events-seeds.js
@@ -229,12 +229,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      */
       return queryInterface.bulkDelete('Events', null, { truncate: true, cascade: true, restartIdentity: true });
   }
 };
